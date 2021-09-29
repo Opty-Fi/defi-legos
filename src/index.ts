eec861acfad8fea48f9f94b44b4488d907d96231
dhruvinparikh <imsilent93@gmail.com>
2021-09-29 18:35:52 -0400

    add dydx

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { default as aavev2 } from "./aavev2"
 import {default as balancer} from "./balancer"
 import {default as compound} from "./compound"
 import {default as curvefi} from "./curvefi"
+import {default as dydx} from "./dydx"
 
 export const legos = {
   aave,
   aavev2,
   balancer,
   compound,
-  curvefi
+  curvefi,
+  dydx
 };
