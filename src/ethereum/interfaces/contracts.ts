03f3c69d6a98a28eb8919cd4fa39f5bc3abeb77c
NouDaimon <peric.iason@gmail.com>
2021-11-24 23:38:53 +0200

    fix: remove IHarvestCodeProvider

--- src/ethereum/interfaces/contracts.ts
+++ src/ethereum/interfaces/contracts.ts
@@ -1,9 +1,7 @@
 import ETHGatewayAbi from "./ETHGateway.json";
-import HarvestCodeProviderAbi from "./HarvestCodeProvider.json";
 
 const contracts = {
     ETHGateway: { abi: ETHGatewayAbi },
-    HarvestCodeProvider: { abi: HarvestCodeProviderAbi }
 };
 
 export default contracts;
