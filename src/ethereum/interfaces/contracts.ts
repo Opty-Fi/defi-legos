880ccaca809ce14d7cce09d7ef07a02bcf406679
NouDaimon <81045441+NouDaimon@users.noreply.github.com>
2021-11-24 23:51:29 +0200

    Release v0.1 iason edit (#21)
    * feat: add yearn(incomplete), dforce(incomplete), harvest(incomplete), Fulcrum, SushiSwap
    
    * feat: harvest finance addition
    
    * feat: add DForce
    
    * feat: add yearn
    
    * feat: add aaveV1/2, harvest, dforce, compound, yearn, yvault
    
    * fix: DForce ABI
    
    * fix: include harvestV2, remove yearn pools
    
    * feat: added all missing contracts and general
    
    * fix: aavev2 json file abi fix
    
    * fix: artifact clashes
    
    * feat: new IVault, IRegistry, DataTypes
    
    * feat: add beefy
    
    * fix: remove datatypes, ivault, iregistry
    
    * fix: import statement  for IAdapterInvestingLimit
    
    * fix: abiencoder added
    
    * fix: remove opty specific interfaces
    
    * fix: remove IHarvestCodeProvider
    
    * fix: remove IETHGateway
    
    Co-authored-by: dhruvinparikh <imsilent93@gmail.com>

--- src/ethereum/interfaces/contracts.ts
+++
@@ -1,9 +0,0 @@
-import ETHGatewayAbi from "./ETHGateway.json";
-import HarvestCodeProviderAbi from "./HarvestCodeProvider.json";
-
-const contracts = {
-    ETHGateway: { abi: ETHGatewayAbi },
-    HarvestCodeProvider: { abi: HarvestCodeProviderAbi }
-};
-
-export default contracts;
