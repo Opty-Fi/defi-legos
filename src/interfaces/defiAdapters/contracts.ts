2acd36e2fa8d54af2171b891bbd71dd3f133a871
dhruvinparikh <imsilent93@gmail.com>
2021-12-20 16:44:35 -0500

    fix(defiadapters): fix module imports

--- src/interfaces/defiAdapters/contracts.ts
+++ src/interfaces/defiAdapters/contracts.ts
@@ -2,7 +2,7 @@ import AdapterAbi from "./abi/Adapter.json";
 import AdapterBorrowAbi from "./abi/AdapterBorrow.json";
 import AdapterFullAbi from "./abi/AdapterFull.json";
 import AdapterHarvestRewardAbi from "./abi/AdapterInvestLimit.json";
-import AdapterStakingAbi from "./abi/Adapterstaking.json";
+import AdapterStakingAbi from "./abi/AdapterStaking.json";
 import AdapterStakingCurveAbi from "./abi/AdapterStakingCurve.json";
 
 const contracts = {
