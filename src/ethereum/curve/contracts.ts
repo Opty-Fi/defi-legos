8b010b844b4284d1c394782572258df071d2bb24
Mario Gutiérrez Valero <m.gutierrez.valero@me.com>
2022-01-10 20:18:34 +0100

    refactor(curve metapool gauges): added curve metapool gauges to curve/contracts.ts (#34)

--- src/ethereum/curve/contracts.ts
+++ src/ethereum/curve/contracts.ts
@@ -6603,6 +6603,38 @@ const contracts = {
       tokens: ["0x605D26FBd5be761089281d5cec2Ce86eeA667109", "0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490"],
     },
   },
+  CurveMetapoolGaugePool: {
+    "alusd3crv-f": {
+      pool: "0x9582C4ADACB3BCE56Fea3e590F05c3ca2fb9C477",
+      lpToken: "0x43b4FdFD4Ff969587185cDB6f0BD875c5Fc83f8c",
+      tokens: ["0x43b4FdFD4Ff969587185cDB6f0BD875c5Fc83f8c"],
+    },
+    "frax3crv-f": {
+      pool: "0x72E158d38dbd50A483501c24f792bDAAA3e7D55C",
+      lpToken: "0xd632f22692FaC7611d2AA1C0D552930D43CAEd3B",
+      tokens: ["0xd632f22692FaC7611d2AA1C0D552930D43CAEd3B"],
+    },
+    "tusd3crv-f": {
+      pool: "0x359FD5d6417aE3D8D6497d9B2e7A890798262BA4",
+      lpToken: "0xEcd5e75AFb02eFa118AF914515D6521aaBd189F1",
+      tokens: ["0xEcd5e75AFb02eFa118AF914515D6521aaBd189F1"],
+    },
+    "lusd3crv-f": {
+      pool: "0x9B8519A9a00100720CCdC8a120fBeD319cA47a14",
+      lpToken: "0xEd279fDD11cA84bEef15AF5D39BB4d4bEE23F0cA",
+      tokens: ["0xEd279fDD11cA84bEef15AF5D39BB4d4bEE23F0cA"],
+    },
+    "busd3crv-f": {
+      pool: "0xd4B22fEdcA85E684919955061fDf353b9d38389b",
+      lpToken: "0x4807862AA8b2bF68830e4C8dc86D0e9A998e085a",
+      tokens: ["0x4807862AA8b2bF68830e4C8dc86D0e9A998e085a"],
+    },
+    "mim-3lp3crv-f": {
+      pool: "0xd8b712d29381748dB89c36BCa0138d7c75866ddF",
+      lpToken: "0x5a6A4D54456819380173272A5E8E9B9904BdF41B",
+      tokens: ["0x5a6A4D54456819380173272A5E8E9B9904BdF41B"],
+    },
+  },
   CurveTokens: {
     hcrv: {
       address: "0xb19059ebb43466C323583928285a49f558E572Fd",
