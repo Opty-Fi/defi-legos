1ba2850280d21dc6e73348e9290f96beb2abb50a
leodinh <leo.atdinh@gmail.com>
2022-01-12 18:14:13 -0500

    added data for aave polygon

---
+++ src/polygon/aave/contracts/IAave.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <=0.9.0;
+pragma experimental ABIEncoderV2;
+
+struct UserAccountData {
+    uint256 totalCollateralETH;
+    uint256 totalDebtETH;
+    uint256 availableBorrowsETH;
+    uint256 currentLiquidationThreshold;
+    uint256 ltv;
+    uint256 healthFactor;
+}
+
+// refer to the whitepaper, section 1.1 basic concepts for a formal description of these properties.
+struct ReserveData {
+    //stores the reserve configuration
+    ReserveConfigurationMap configuration;
+    //the liquidity index. Expressed in ray
+    uint128 liquidityIndex;
+    //variable borrow index. Expressed in ray
+    uint128 variableBorrowIndex;
+    //the current supply rate. Expressed in ray
+    uint128 currentLiquidityRate;
+    //the current variable borrow rate. Expressed in ray
+    uint128 currentVariableBorrowRate;
+    //the current stable borrow rate. Expressed in ray
+    uint128 currentStableBorrowRate;
+    uint40 lastUpdateTimestamp;
+    //tokens addresses
+    address aTokenAddress;
+    address stableDebtTokenAddress;
+    address variableDebtTokenAddress;
+    //address of the interest rate strategy
+    address interestRateStrategyAddress;
+    //the id of the reserve. Represents the position in the list of the active reserves
+    uint8 id;
+}
+
+struct ReserveConfigurationMap {
+    //bit 0-15: LTV
+    //bit 16-31: Liq. threshold
+    //bit 32-47: Liq. bonus
+    //bit 48-55: Decimals
+    //bit 56: Reserve is active
+    //bit 57: reserve is frozen
+    //bit 58: borrowing is enabled
+    //bit 59: stable rate borrowing enabled
+    //bit 60-63: reserved
+    //bit 64-79: reserve factor
+    uint256 data;
+}
+
+  struct UserConfigurationMap {
+    uint256 data;
+  }
+
+interface IAave {
+    function deposit(
+        address _asset,
+        uint256 _amount,
+        address _onBehalfOf,
+        uint16 _referralCode
+    ) external;
+
+    function setUserUseReserveAsCollateral(address _asset, bool _useAsCollateral) external;
+
+    function withdraw(
+        address _asset,
+        uint256 _amount,
+        address _to
+    ) external;
+
+    function borrow(
+        address _asset,
+        uint256 _amount,
+        uint256 _interestRateMode,
+        uint16 _referralCode,
+        address _onBehalfOf
+    ) external;
+
+    function repay(
+        address _asset,
+        uint256 _amount,
+        uint256 _rateMode,
+        address _onBehalfOf
+    ) external;
+
+    function getUserAccountData(address _user) external view returns (UserAccountData memory);
+
+    function getConfiguration(address _asset) external view returns (ReserveConfigurationMap memory);
+
+    function getUserConfiguration(address user) external view returns (UserConfigurationMap memory);
+
+    function getReserveData(address _asset) external view returns (ReserveData memory);
+
+    function paused() external view returns (bool);
+
+    function getReservesList() external view returns (address[] memory);
+
+    function getAddressesProvider() external view returns (address);
+
+    function getReserveNormalizedIncome(address _asset) external view returns (uint256);
+
+    function getReserveNormalizedVariableDebt(address _asset) external view returns (uint256);
+}
