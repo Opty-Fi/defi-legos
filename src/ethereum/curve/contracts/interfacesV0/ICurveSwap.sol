7ca9bdffa547ba8b9784b1fecac7d54266d486e3
Mario Gutiérrez Valero <m.gutierrez.valero@me.com>
2022-02-08 09:44:21 +0100

    refactor(icurve-swap): added coins(int128) function in ICurveSwap.sol

--- src/ethereum/curve/contracts/interfacesV0/ICurveSwap.sol
+++ src/ethereum/curve/contracts/interfacesV0/ICurveSwap.sol
@@ -33,6 +33,8 @@ interface ICurveSwap {
 
     function get_virtual_price() external view returns (uint256);
 
+    function coins(int128 arg0) external view returns (address);
+
     function coins(uint256 arg0) external view returns (address);
 
     function underlying_coins(uint256 arg0) external view returns (address);
