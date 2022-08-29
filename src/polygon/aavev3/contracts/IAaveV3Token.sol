fca1e1821a2ecc98de3cbe7cd114094fbaf7dc78
Leo Tuan Dinh <54964744+leodinh@users.noreply.github.com>
2022-08-29 15:09:14 -0400

    feat: added aave v3 on polygon (#63)

---
+++ src/polygon/aavev3/contracts/IAaveV3Token.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <=0.9.0;
+
+interface IAaveV3Token {
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event BalanceTransfer(address indexed from, address indexed to, uint256 value, uint256 index);
+    event Burn(address indexed from, address indexed target, uint256 value, uint256 index);
+    event Mint(address indexed from, uint256 value, uint256 index);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function ATOKEN_REVISION() external view returns (uint256);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+
+    function EIP712_REVISION() external view returns (bytes memory);
+
+    function PERMIT_TYPEHASH() external view returns (bytes32);
+
+    function POOL() external view returns (address);
+
+    function RESERVE_TREASURY_ADDRESS() external view returns (address);
+
+    function UNDERLYING_ASSET_ADDRESS() external view returns (address);
+
+    function _nonces(address) external view returns (uint256);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function decimals() external view returns (uint8);
+
+    function getScaledUserBalanceAndSupply(address user) external view returns (uint256, uint256);
+
+    function balanceOf(address user) external view returns (uint256);
+
+    function name() external view returns (string memory);
+
+    function scaledBalanceOf(address user) external view returns (uint256);
+
+    function scaledTotalSupply() external view returns (uint256);
+
+    function symbol() external view returns (string memory);
+
+    function totalSupply() external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function burn(
+        address user,
+        address receiverOfUnderlying,
+        uint256 amount,
+        uint256 index
+    ) external;
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool);
+
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
+
+    function initialize(
+        uint8 underlyingAssetDecimals,
+        string memory tokenName,
+        string memory tokenSymbol
+    ) external;
+
+    function mint(
+        address user,
+        uint256 amount,
+        uint256 index
+    ) external returns (bool);
+
+    function mintToTreasury(uint256 amount, uint256 index) external;
+
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    function transferOnLiquidation(
+        address from,
+        address to,
+        uint256 value
+    ) external;
+
+    function transferUnderlyingTo(address target, uint256 amount) external returns (uint256);
+}
